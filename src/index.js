7606c4e1c368a5bf74e99f4f7c980b7512f276fb
Bill <xuyatao415@gmail.com>
2021-11-22 13:08:40 +0800

    chore: rebuild

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom";
 import React from "react";
 
-class NameForm extends React.Component {
+class FlavorForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { value: 'coconut' };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,7 +15,7 @@ class NameForm extends React.Component {
   }
 
   handleSubmit(event) {
-    alert('A name was submitted: ' + this.state.value);
+    alert('Your favorite flavor is: ' + this.state.value);
     event.preventDefault();
   }
 
@@ -23,8 +23,13 @@ class NameForm extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          Pick your favorite flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
         </label>
         <input type="submit" value="Submit" />
       </form>
@@ -33,6 +38,6 @@ class NameForm extends React.Component {
 }
 
 ReactDOM.render(
-  <NameForm />,
+  <FlavorForm />,
   document.getElementById('root')
 );
