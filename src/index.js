f468dbf6f6573ab0dbe3b1626837326d44639366
xuyataobj <xuyataobj@yuanfudao.com>
2021-12-25 17:29:19 +0800

    chore: 有状态组件

--- src/index.js
+++ src/index.js
@@ -5,22 +5,41 @@ import reportWebVitals from './reportWebVitals';
 
 
 
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
 
-function App() {
-  return (
-    <div>
-      <Welcome name="Sara" />
-      <Welcome name="Cahal" />
-      <Welcome name="Edite" />
-    </div>
-  );
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(
-  <App />,
+  <Clock />,
   document.getElementById('root')
 );
 
