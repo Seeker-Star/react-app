25020182e36a16ec573916a869699ee2d2981b58
xuyataobj <xuyataobj@yuanfudao.com>
2021-12-31 19:07:34 +0800

    add: add Jenkinsfile

--- src/index.js
+++ src/index.js
@@ -31,8 +31,8 @@ class Clock extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        <h1>北京时间</h1>
+        <h2>{this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
